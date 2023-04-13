846b6a8475346ca71a53a53b6a51fa08c4cae87f
Maks Bidziura <maksbidzura@gmail.com>
2023-04-13 15:27:08 +0300

    test_checkbox_create

--- src/test/java/demoqa/TestTextBox.java
+++ src/test/java/demoqa/TestTextBox.java
@@ -1,4 +1,4 @@
-package textbox;
+package demoqa;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -9,24 +9,18 @@ import org.testng.Assert;
 public class TestTextBox {
 
     @Test
-    public void checkSubmit(){
+    public void checkSubmit() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://demoqa.com/text-box");
+
         driver.findElement(By.xpath("//input[@id='userName']")).sendKeys(" Maksum");
         driver.findElement(By.xpath("//input[@id='userEmail']")).sendKeys(" Maksum@gmail.com");
         driver.findElement(By.xpath("//textarea[@id='currentAddress']")).sendKeys(" adress");
         driver.findElement(By.xpath("//textarea[@id='permanentAddress']")).sendKeys(" permanent adress");
-
         driver.findElement(By.xpath("//button[@id='submit']")).click();
-
         Assert.assertTrue(driver.findElement(By.xpath("//*[@id='name']")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.xpath("//*[@id='email']")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.xpath("//p[@id='currentAddress']")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.xpath("//p[@id='permanentAddress']")).isDisplayed());
-
-
-
-
     }
-
 }
